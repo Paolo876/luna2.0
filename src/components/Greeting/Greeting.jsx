970a98b1a8ad68fba6ce3c624ce5830940412c41
paoloBugarin <paolopaolobugarin@gmail.com>
2023-05-31 23:46:30 -0700

    updated greeting component

--- src/components/Greeting/Greeting.jsx
+++ src/components/Greeting/Greeting.jsx
@@ -1,18 +1,40 @@
 import { useRef } from "react";
 import ComponentContainer from '../UI/ComponentContainer'
 import useUserRedux from '../../hooks/useUserRedux'
+import useSettingsRedux from "../../hooks/useSettingsRedux";
 import { Typography } from '@mui/material'
 
 const Greeting = () => {
+  const ref = useRef();   //for react-draggable
   const { name } = useUserRedux();
+  const { components } = useSettingsRedux();
+  const addedStyles = components.find(item => item.name === "greeting").addedStyles;
+  const hours = new Date().getHours();
+
+  let greeting;
+  if(hours < 12){
+      greeting = 'Good morning';
+  } else if(hours >= 12 && hours <= 18){
+      greeting = 'Good afternoon';
+  } else {
+      greeting = 'Good evening';
+  }
+
   return (
     <ComponentContainer 
       additionalStyles={{
         top: "32%",
         background: "radial-gradient(rgba(0, 0, 0, 0.1) 10%, transparent 60%)",
-
+        fontSize: "5em",
+        textShadow: "0 0 10px rgba(0, 0, 0, 0.4)",
+        ...addedStyles
+        
       }}
-      >Greeting</ComponentContainer>
+      ref={ref} 
+      id="greeting"
+      >
+      <p>{ `${greeting}, ${name}` }!</p>
+      </ComponentContainer>
   )
 }
 
