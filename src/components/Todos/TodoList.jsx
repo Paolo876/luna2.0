b858d2772ba077a361fc7dcd75900dfdc04be2b7
paoloBugarin <paolopaolobugarin@gmail.com>
2023-06-02 23:32:00 -0700

    updated TodoList component

--- src/components/Todos/TodoList.jsx
+++ src/components/Todos/TodoList.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import useTodoRedux from '../../hooks/useTodoRedux'
-import { Stack } from '@mui/material';
+import { ButtonBase, List, ListItem, ListItemButton, ListItemText, Typography } from '@mui/material';
 
 
 const TodoList = () => {
   const { items } = useTodoRedux();
   console.log(items)
   return (
-    <Stack
+    <List
       sx={{
         borderBottom: "1px solid rgba(0, 0, 0, 0.1)",
-        p: ".5em",
+        // p: ".5em",
       }}
     >
-    
-    </Stack>
+      {items.map(item => <ListItem key={item.id}>
+        
+        {/* <ButtonBase sx={{height: "100%", width: "100%", textAlign: "left"}}><Typography variant="body2" textAlign="left">{item.text}</Typography></ButtonBase> */}
+      </ListItem>)}
+    </List>
   )
 }
 
