10d92fa8905ad85741c16eb84ee03847ebdafab1
paoloBugarin <paolopaolobugarin@gmail.com>
2023-06-03 17:10:59 -0700

    updated TodoList ui

--- src/components/Todos/TodoList.jsx
+++ src/components/Todos/TodoList.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import useTodoRedux from '../../hooks/useTodoRedux'
-import { Checkbox, List, ListItem, ListItemButton, ListItemText, Typography, IconButton, ListItemIcon, Box } from '@mui/material';
+import { Checkbox, List, ListItem, ListItemButton, ListItemText, Typography, IconButton, ListItemIcon, Box, TextField } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 import CloseIcon from '@mui/icons-material/Close';
 
@@ -21,7 +21,7 @@ const TodoList = () => {
           key={item.id}
           secondaryAction={
             <Box>
-              <IconButton edge="end" aria-label="comments" sx={{mr: .01}}>
+              <IconButton edge="end" aria-label="comments" sx={{mr: .01, opacity: item.isFinished ? .25 : 1, }} disabled={item.isFinished} >
                 <EditIcon color="primary" fontSize="small"/>
               </IconButton>
               <IconButton edge="end" aria-label="comments">
@@ -39,13 +39,29 @@ const TodoList = () => {
                 checked={item.isFinished}
                 // tabIndex={-1}
                 // disableRipple
-                inputProps={{ 'aria-labelledby': item.id }}
+                inputProps={{ 'aria-labelledby': item.id}}
+                sx={{opacity: item.isFinished ? 1 : .35}}
               />
             </ListItemIcon>
             <ListItemText 
               id={item.id} 
-              primary={<Typography variant="body2" fontSize={15} letterSpacing={.5} fontWeight={300}>{item.text}</Typography>} 
+              primary={<Typography 
+                variant="body2" 
+                letterSpacing={.5} 
+                fontWeight={300}
+                sx={{
+                  transition: "all .1s linear",
+                  fontFamily: "Inter",
+                  fontStyle: item.isFinished ? "italic" : "initial",
+                  textDecoration: item.isFinished ? "line-through" : "initial",
+                  opacity: item.isFinished ? .75 : 1,
+                  transform: item.isFinished ? "translateX(0.25em)" : "none",
+                }}
+              >
+                {item.text}
+              </Typography>} 
             />
+            {/* <TextField size='small' variant="standard"/> */}
           </ListItemButton>
         </ListItem>
       ))}
