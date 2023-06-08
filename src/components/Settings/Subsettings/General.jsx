212ec529d5c342fc989fd6d494c835e4362278af
paoloBugarin <paolopaolobugarin@gmail.com>
2023-06-08 00:00:03 -0700

    updated General settings ui

--- src/components/Settings/Subsettings/General.jsx
+++ src/components/Settings/Subsettings/General.jsx
@@ -3,7 +3,7 @@ import SubsettingContainer from './SubsettingContainer'
 import { Box, TextField, Typography, Select, FormControl, InputLabel, Button, InputAdornment } from '@mui/material'
 import useSettingsRedux from "../../../hooks/useSettingsRedux";
 import useUserRedux from "../../../hooks/useUserRedux"
-
+import validateInput from '../../../utils/validate-input';
 
 const labelStyles = {
   fontSize: 20,
@@ -15,10 +15,17 @@ const General = () => {
   const { name, setUserName } = useUserRedux();
   const { dateFormat } = useSettingsRedux();
   const [ nameInput, setNameInput ] = useState(name)
+  const [ nameError, setNameError ] = useState({state: false, value: null})
 
   
   const handleNameSubmit = () => {
-    setUserName(nameInput)
+    if(validateInput(nameInput, "text")) {
+      setNameError({state: false, value: null});
+      setUserName(nameInput);
+    } else {
+      setNameError({state: true, value:"Please enter a valid name."})
+      // setNameInput("")
+    }
   }
 
   useEffect(() => {
@@ -28,44 +35,48 @@ const General = () => {
   return ( 
     <SubsettingContainer title="General">
       <Box>
-        <TextField 
-          type="text"
-          size='small' 
-          variant="standard" 
-          fullWidth 
-          inputProps={{maxLength: 15}} 
-          label={<Typography variant="body2" sx={labelStyles}>Change Display Name</Typography>} 
-          InputLabelProps={{shrink: true}}
-          value={nameInput}
-          onChange={e => setNameInput(e.target.value)}
-          onKeyDown={e => { if(e.key === "Enter") handleNameSubmit()}}
-          InputProps={{ endAdornment: (
-          <InputAdornment position="end">
-            {nameInput !== name && (
-              <Button 
-                type="button"
-                size="small" 
-                variant="contained" 
-                onClick={handleNameSubmit}
-                sx={{py: .25, px: 1.5, fontSize: 9, fontWeight: 400, letterSpacing: .15, mr: .5, opacity: .8}}
-              >
-                Save Changes
-              </Button>
-            )}
-          </InputAdornment>
-        )}}
-          sx={{
-            mb: 3,
-            '.MuiInputBase-input': { 
-                fontWeight: 500,
-                px: .5,
-                py: .25,
-                mt: 1,
-                fontSize: ".85em",
-                letterSpacing: .75,
-              },
-        }}
-        />
+        <Box mb={3}>
+          <TextField 
+            type="text"
+            size='small' 
+            variant="standard" 
+            fullWidth 
+            inputProps={{maxLength: 15}} 
+            label={<Typography variant="body2" sx={labelStyles}>Change Display Name</Typography>} 
+            InputLabelProps={{shrink: true}}
+            value={nameInput}
+            error={nameError.state}
+            onChange={e => setNameInput(e.target.value)}
+            onKeyDown={e => { if(e.key === "Enter") handleNameSubmit()}}
+            InputProps={{ endAdornment: (
+              <InputAdornment position="end">
+                {nameInput !== name && (
+                  <Button 
+                    type="button"
+                    size="small" 
+                    variant="contained" 
+                    onClick={handleNameSubmit}
+                    sx={{py: .25, px: 1.5, fontSize: 9, fontWeight: 400, letterSpacing: .15, mr: .5, opacity: .8}}
+                  >
+                    Save Changes
+                  </Button>
+                )}
+              </InputAdornment>
+            )}}
+            sx={{
+              '.MuiInputBase-input': { 
+                  fontWeight: 500,
+                  px: .5,
+                  py: .25,
+                  mt: 1,
+                  fontSize: ".85em",
+                  letterSpacing: .75,
+                },
+            }}
+          />
+          {nameError.state && <Typography variant="body2" color="error.light" mt={.15} fontStyle="italic" fontSize={12} letterSpacing={.25} sx={{opacity: .65}}>{nameError.value}</Typography>}
+        </Box>
+        
         {/* <FormControl  variant="standard" sx={{ mb: 3, width: "100%" }}>
           <InputLabel shrink={true}><Typography variant="body2" sx={labelStyles}>Change Time Format</Typography></InputLabel>
           <Select
