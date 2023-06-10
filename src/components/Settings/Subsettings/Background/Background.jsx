9a250c5d963c03865903385d191cff4eba5280e8
paoloBugarin <paolopaolobugarin@gmail.com>
2023-06-09 22:58:18 -0700

    updated background settings component

--- src/components/Settings/Subsettings/Background/Background.jsx
+++ src/components/Settings/Subsettings/Background/Background.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import SubsettingContainer from '../SubsettingContainer'
+import { FormControl, FormControlLabel, Radio, RadioGroup, Box, Typography } from '@mui/material'
 
 
 const Background = () => {
   return (
     <SubsettingContainer title="Background">
-        sdsd
+      <Box>
+        <Typography variant="body2" sx={{opacity: .8, mt: 1, fontSize: 13, }}>
+          Note: Random background images are generated on reload if no image is set as default.
+        </Typography>
+      </Box>
+      <FormControl>
+        <RadioGroup
+          // value={value}
+          // onChange={handleChange}
+        >
+          <FormControlLabel value="local" control={<Radio />} label="Saved Backgrounds" />
+          <FormControlLabel value="fetch" control={<Radio />} label="Fetch Random Backgrounds" />
+        </RadioGroup>
+      </FormControl>
     </SubsettingContainer>
   )
 }
