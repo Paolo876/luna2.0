5b0d56f1a5348ed57a18c33453b2bb7c4b18abfb
paoloBugarin <paolopaolobugarin@gmail.com>
2023-06-11 15:46:34 -0700

    updated component settings ui

--- src/components/Settings/Subsettings/Components/Components.jsx
+++ src/components/Settings/Subsettings/Components/Components.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import { useState } from 'react'
 import SubsettingContainer from "../SubsettingContainer"
-
+import { Button, Menu, MenuItem, Box } from '@mui/material'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 const Components = () => {
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [ selectedComponent, setSelectedComponent ] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+
   return (
-    <SubsettingContainer title="Components">Components</SubsettingContainer>
+    <SubsettingContainer title="Components">
+      <Box>
+      <Button
+        id="basic-button"
+        aria-controls={anchorEl ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={anchorEl ? 'true' : undefined}
+        onClick={handleClick}
+        endIcon={<KeyboardArrowDownIcon/>}
+        sx={{width: 350, display: "flex", justifyContent: "space-between", background: "rgba(15, 15, 15, .75)"}}
+        variant="outlined"
+      >
+        Select Component
+      </Button>
+      </Box>
+    </SubsettingContainer>
   )
 }
 
