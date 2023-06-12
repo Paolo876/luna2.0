03a27d9899d06d0b996597a406e90beeb071ce77
paoloBugarin <paolopaolobugarin@gmail.com>
2023-06-11 20:25:37 -0700

    created selectedComponentSettings

--- src/components/Settings/Subsettings/Components/Components.jsx
+++ src/components/Settings/Subsettings/Components/Components.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import useSettingsRedux from "../../../../hooks/useSettingsRedux";
 import SubsettingContainer from "../SubsettingContainer";
-import { Button, Menu, MenuItem, Box, Typography } from '@mui/material';
+import { Button, Menu, MenuItem, Box, Typography, Divider } from '@mui/material';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import SelectedComponentSettings from './SelectedComponentSettings';
 
 const Components = () => {
   const { components } = useSettingsRedux();
@@ -15,13 +16,13 @@ const Components = () => {
   };
 
   const handleItemClick = (item) => {
-    setSelectedComponent(item)
+    setSelectedComponent(item.value)
     setAnchorEl(null)
   }
   console.log(selectedComponent)
   return (
     <SubsettingContainer title="Components">
-      <Box>
+      <Box mb={1}>
         <Button
           id="basic-button"
           aria-controls={anchorEl ? 'basic-menu' : undefined}
@@ -35,31 +36,33 @@ const Components = () => {
           {selectedComponent === null ? 'Select Component' : selectedComponent}
         </Button>
         <Menu
-        id="basic-menu"
-        anchorEl={anchorEl}
-        open={anchorEl !== null}
-        onClose={() => setAnchorEl(null)}
-        MenuListProps={{
-          'aria-labelledby': 'basic-button',
-          sx:{
-            background: "rgba(0,0,0,0)",
-            p:.25,
-            width: 360
-          }
-        }}
-        sx={{mx: .25, my: .25}}
-      >
-        {components.map(item => (
-          <MenuItem 
-            key={item.name}
-            onClick={() => handleItemClick(item.value)}
-            sx={{width: "100%", color: "black", }}
-          >
-            <Typography variant="body2" sx={{fontWeight: 600, letterSpacing: .5, opacity: .85, fontSize: 15}}>{item.value}</Typography>
-          </MenuItem>
-        ))}
-      </Menu>
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={anchorEl !== null}
+          onClose={() => setAnchorEl(null)}
+          MenuListProps={{
+            'aria-labelledby': 'basic-button',
+            sx:{
+              background: "rgba(0,0,0,0)",
+              p:.25,
+              width: 360
+            }
+          }}
+          sx={{ mx: .25, my: .25 }}
+        >
+          {components.map(item => (
+            <MenuItem 
+              key={item.name}
+              onClick={() => handleItemClick({name: item.name, value: item.value})}
+              sx={{width: "100%", color: "black", }}
+            >
+              <Typography variant="body2" sx={{fontWeight: 600, letterSpacing: .5, opacity: .85, fontSize: 15}}>{item.value}</Typography>
+            </MenuItem>
+          ))}
+        </Menu>
       </Box>
+      <Divider/>
+      <SelectedComponentSettings component={selectedComponent}/>
     </SubsettingContainer>
   )
 }
---
+++ src/components/Settings/Subsettings/Components/SelectedComponentSettings.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Box } from '@mui/material'
+
+
+const SelectedComponentSettings = ({ component }) => {
+  return (
+    <div>SelectedComponentSettings</div>
+  )
+}
+
+export default SelectedComponentSettings
