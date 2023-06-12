ad907b8f91c9aaec41cd5e318a79affae9e329e6
paoloBugarin <paolopaolobugarin@gmail.com>
2023-06-12 00:53:09 -0700

    updated Interface settings component

--- src/components/Settings/Subsettings/Interface.jsx
+++ src/components/Settings/Subsettings/Interface.jsx
@@ -1,31 +1,50 @@
 import React from 'react'
 import SubsettingContainer from './SubsettingContainer'
 import useSettingsRedux from '../../../hooks/useSettingsRedux'
-import { Box, Typography } from '@mui/material'
+import { Box, Slider, Typography, Tooltip } from '@mui/material'
 import { MuiColorInput } from 'mui-color-input'
 import { rgbToHex } from '@mui/material'
-import hexToRgb from "../../../utils/hex-to-rgb"
 
 const labelStyles = {
   fontSize: 15,
   letterSpacing:.5, 
 }
 
+const valueLabelComponent = ({ children, value }) => (
+  <Tooltip enterTouchDelay={0} placement="top" title={value}>
+    {children}
+  </Tooltip>
+);
+
 
 const Interface = () => {
   const { ui, changeContainerColor } = useSettingsRedux();
   const [r, g ,b, a] = ui.containerColor.substr(21).split(", ", 4);
 
-
+  // console.log(r, g ,b, a)
   return (
     <SubsettingContainer title="Interface">
       <Box>
         <Typography variant="body2" fontSize={18} letterSpacing={.5}>Container Color</Typography>
 
-        <Box sx={{display: "flex", justifyContent: "space-between", alignItems: "center"}} my={2}>
+        <Box sx={{display: "flex", justifyContent: "space-between", alignItems: "center"}} my={2} ml={1.5}>
           <Typography variant="body2" sx={labelStyles}>Color</Typography>
           <MuiColorInput value={rgbToHex(`rgb(${r},${g},${b})`)} onChange={(e) => changeContainerColor({color: e, alpha: parseFloat(a)})} size='small' sx={{fontSize: 13, letterSpacing: .5}}/>
-
+        </Box>
+        <Box sx={{display: "flex", justifyContent: "space-between", alignItems: "center"}} my={2} ml={1.5}>
+          <Typography variant="body2" sx={labelStyles}>Alpha</Typography>
+          <Slider
+            sx={{width: 290, ml: "auto", mr:1}}
+            min={.1}
+            max={1}
+            step={.1}
+            valueLabelDisplay="auto"
+            value={parseFloat(a)}
+            onChange={(e) => changeContainerColor({color: `rgb(${r},${g},${b})`, alpha: e.target.value})}
+            slots={{
+              valueLabel: valueLabelComponent,
+            }}
+          />
         </Box>
       </Box>
     </SubsettingContainer>
--- src/redux/reducers/settingsSlice.js
+++ src/redux/reducers/settingsSlice.js
@@ -100,13 +100,11 @@ const settingsSlice = createSlice({
         // // UI
 
         changeContainerColor(state, {payload}){
-            const colors = payload.color
-            const [r, g ,b] = colors.substr(4).split(", ", 4);
-            console.log(r)
-            console.log(g)
-            console.log(b)
-            // const value = `linear-gradient(rgba(${colors.r}, ${colors.g}, ${colors.b}, ${payload.alpha}), rgba(${colors.r}, ${colors.g}, ${colors.b}, ${payload.alpha}))`;
-            // state.ui.containerColor = value;
+            const colors = payload.color.substr(0, payload.color.length - 1).substr(4)
+            const [ r, g, b ] = colors.split(", ")
+            console.log(r, g ,b )
+            const value = `linear-gradient(rgba(${colors}, ${payload.alpha}), rgba(${colors}, ${payload.alpha}))`;
+            state.ui.containerColor = value;
             // localStorage.setItem('uiConfig', JSON.stringify(state.ui))
         },
         // changeSettingsButtonPosition(state, {payload}){
