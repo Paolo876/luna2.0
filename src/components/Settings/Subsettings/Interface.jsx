a65066cef1d7649f8996806a2d5b1dd72761cdc4
paoloBugarin <paolopaolobugarin@gmail.com>
2023-06-12 22:44:21 -0700

    updated Interface component

--- src/components/Settings/Subsettings/Interface.jsx
+++ src/components/Settings/Subsettings/Interface.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import SubsettingContainer from './SubsettingContainer'
-import useSettingsRedux from '../../../hooks/useSettingsRedux'
 import useUiRedux from '../../../hooks/useUiRedux'
-import { Box, Slider, Typography, Tooltip } from '@mui/material'
+import { Box, Slider, Typography, Tooltip, Button } from '@mui/material'
 import { MuiColorInput } from 'mui-color-input'
-import { rgbToHex } from '@mui/material'
-import hexToRgb from "../../../utils/hex-to-rgb"
 
 
 const labelStyles = {
-  fontSize: 15,
+  fontSize: 16,
   letterSpacing:.5, 
 }
 
@@ -21,22 +18,21 @@ const valueLabelComponent = ({ children, value }) => (
 
 
 const Interface = () => {
-  const { interface: { containerColor }, changeContainerColor } = useUiRedux();
+  const { interface: { containerColor, primaryColor }, changeContainerColor } = useUiRedux();
   const [r, g ,b, a] = containerColor.substr(21).split(", ", 4);
 
   return (
     <SubsettingContainer title="Interface">
-      <Box pr={4}>
-        <Typography variant="body2" fontSize={18} letterSpacing={.5}>Container Color</Typography>
-
-        <Box sx={{display: "flex", justifyContent: "space-between", alignItems: "center"}} my={2} ml={1.5}>
-          <Typography variant="body2" sx={labelStyles}>Color</Typography>
+      <Box pr={4} mt={1} mb={3}>
+        <Typography variant="body2" sx={labelStyles}>Container Color</Typography>
+        <Box sx={{display: "flex", justifyContent: "space-between", alignItems: "center"}} my={1.75} ml={1.5}>
+          <Typography variant="body2" fontSize={13}>Color</Typography>
           <MuiColorInput value={`rgb(${r},${g},${b})`} onChange={(e) => changeContainerColor({color: e, alpha: parseFloat(a)})} size='small' sx={{fontSize: 13, letterSpacing: .5}}/>
         </Box>
-        <Box sx={{display: "flex", justifyContent: "space-between", alignItems: "center"}} my={2} ml={1.5}>
-          <Typography variant="body2" sx={labelStyles}>Alpha</Typography>
+        <Box sx={{display: "flex", justifyContent: "space-between", alignItems: "center"}} my={1.75} ml={1.5}>
+          <Typography variant="body2" fontSize={13}>Alpha</Typography>
           <Slider
-            sx={{width: 290, ml: "auto", mr:1}}
+            sx={{width: 225, ml: "auto", mr:1}}
             min={.1}
             max={1}
             step={.02}
@@ -48,6 +44,28 @@ const Interface = () => {
             }}
           />
         </Box>
+        <Box sx={{display: "flex", justifyContent: "space-between", alignItems: "center"}} my={1.75} ml={1.5}>
+          <Typography variant="body2" fontSize={13}>Backdrop Blur</Typography>
+          <Slider
+            sx={{width: 225, ml: "auto", mr:1}}
+            min={0}
+            max={15}
+            step={1}
+            valueLabelDisplay="auto"
+            // value={parseFloat(a)}
+            // onChange={(e) => changeContainerColor({color: `rgb(${r},${g},${b})`, alpha: e.target.value})}
+            slots={{
+              valueLabel: valueLabelComponent,
+            }}
+          />
+        </Box>
+      </Box>
+      <Box sx={{display: "flex", justifyContent: "space-between", alignItems: "center"}} >
+        <Typography variant="body2" sx={labelStyles}>Primary Color</Typography>
+        <MuiColorInput value={primaryColor} size='small' sx={{fontSize: 13, letterSpacing: .5}}/>
+      </Box>
+      <Box sx={{display: "flex", justifyContent: "right", px: 3, mt: "auto"}}>
+        <Button color="warning" variant="contained" size="small" onClick={() => console.log("reset")} sx={{fontSize: 12}}>Reset To Default</Button>
       </Box>
     </SubsettingContainer>
   )
