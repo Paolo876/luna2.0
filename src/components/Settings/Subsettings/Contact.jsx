44b7cb3864710c2881d67b891843ab0ef153defd
paoloBugarin <paolopaolobugarin@gmail.com>
2023-06-15 00:45:07 -0700

    updated contact settings ui

--- src/components/Settings/Subsettings/Contact.jsx
+++ src/components/Settings/Subsettings/Contact.jsx
@@ -21,13 +21,13 @@ const Contact = () => {
       </Box>
       <Box mb={5}>
         <Typography variant="h4" sx={{letterSpacing: 1, fontWeight: 100, opacity: .9, fontSize: 15}}>Email</Typography>
-        <ButtonBase target="_blank" href="mailto: paolopaolobugarin@gmail.com" sx={{ px:1 }}>
+        <ButtonBase target="_blank" href="mailto: paolopaolobugarin@gmail.com" sx={{ px:1 }} draggable={false}>
           <Typography variant="h6" sx={{letterSpacing: .5, opacity: 1, fontWeight: 500, fontSize: 16, mt: 1 }} color="primary">paolopaolobugarin@gmail.com</Typography>
         </ButtonBase>      
       </Box>
       <Box mb={5}>
         <Typography variant="h4" sx={{letterSpacing: 1, fontWeight: 100, opacity: .9, fontSize: 15}}>Phone</Typography>
-        <ButtonBase href="tel:3234817852" sx={{ px:1 }}>
+        <ButtonBase href="tel:3234817852" sx={{ px:1 }} draggable={false}>
           <Typography variant="body1" sx={{letterSpacing: .5, opacity: 1, fontWeight: 500, fontSize: 16, mt: 1 }} color="primary">+1 (323) 481 7852</Typography>
         </ButtonBase>      
       </Box>
@@ -35,17 +35,17 @@ const Contact = () => {
         <Typography variant="h4" sx={{letterSpacing: 1, fontWeight: 100, opacity: .9, fontSize: 15}}>Social</Typography>
         <Box sx={{display: "flex", gap: 3, flexDirection: "row", width: "100%", mt: 1.75, pl: 1}}>
           <Tooltip title="Github" arrow enterDelay={500} placement="bottom">
-            <IconButton size="small" target="_blank" href="https://github.com/Paolo876" sx={{mixBlendMode: "difference"}} disableTouchRipple>
+            <IconButton draggable={false} size="small" target="_blank" href="https://github.com/Paolo876" sx={{mixBlendMode: "difference"}} disableTouchRipple>
               <GitHubIcon fontSize="medium" color="primary"/>
             </IconButton>
           </Tooltip>
           <Tooltip title="LinkedIn" arrow enterDelay={500} placement="bottom">
-            <IconButton size="small" target="_blank" href="https://www.linkedin.com/in/paolo-bugarin/" sx={{mixBlendMode: "difference"}} disableTouchRipple>
+            <IconButton draggable={false} size="small" target="_blank" href="https://www.linkedin.com/in/paolo-bugarin/" sx={{mixBlendMode: "difference"}} disableTouchRipple>
               <LinkedInIcon fontSize="medium" color="primary"/>
             </IconButton>
           </Tooltip>
           <Tooltip title="Facebook" arrow enterDelay={500} placement="bottom">
-            <IconButton size="small" target="_blank" href="https://www.facebook.com/paolobugarin19/" sx={{mixBlendMode: "difference"}} disableTouchRipple>
+            <IconButton draggable={false} size="small" target="_blank" href="https://www.facebook.com/paolobugarin19/" sx={{mixBlendMode: "difference"}} disableTouchRipple>
               <FacebookIcon fontSize="medium" color="primary"/>
             </IconButton>
           </Tooltip>
