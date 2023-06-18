487a0fab4a2c95cb2b5e27a0c25efbb689b756f1
paoloBugarin <paolopaolobugarin@gmail.com>
2023-06-17 22:12:59 -0700

    installed firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDqNB8BjQ2FVCTBFIGqgUf2jIQ3IzeCxCU",
+  authDomain: "luna2-3385e.firebaseapp.com",
+  projectId: "luna2-3385e",
+  storageBucket: "luna2-3385e.appspot.com",
+  messagingSenderId: "391866126689",
+  appId: "1:391866126689:web:ba48b05805d8894172f520"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
